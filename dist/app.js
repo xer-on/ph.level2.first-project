eac893c4b1b22783785a2409de10eb5e3474c2af
xeron <rifat.rahman378@gmail.com>
2024-12-28 04:25:51 +0600

    1

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,16 @@
 "use strict";
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// const port = 3000;
+//parser
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
-console.log(process.cwd());
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  const a: number = 10;
-  
-  res.send(a);
+  res.send("Hello World");
 });
 
 export default app;
