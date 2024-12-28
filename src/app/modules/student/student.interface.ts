7d1a810bb67cadf2ad1d248c476e018ed3a72e68
xeron <rifat.rahman378@gmail.com>
2024-12-28 18:35:04 +0600

    schema and interface added

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+// 1. Create an interface representing a document in MongoDB.
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNumber: string;
+  emergencyContactNumber: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: 'active' | 'blocked';
+};
