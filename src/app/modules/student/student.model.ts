8a74d3fa19a66c14b08f3003dd4efc7ebf31de80
xeron <rifat.rahman378@gmail.com>
2024-12-31 03:52:15 +0600

    added validation

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -29,23 +29,40 @@ const localGuardianSchema = new Schema<LocalGuardian>({
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String },
-  name: userNameSchema,
-  gender: { type: String, enum: ['male', 'female'] },
+  id: { type: String, unique: true, required: true },
+  name: { type: userNameSchema, required: true },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '{VALUE} is not a valid gender',
+    },
+    required: true,
+  },
   dateOfBirth: { type: String },
-  email: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   contactNumber: { type: String, required: true },
   emergencyContactNumber: { type: String, required: true },
   bloodGroup: {
     type: String,
-    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+      message: '{VALUE} is not a valid blood group',
+    },
   },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
+  guardian: { type: guardianSchema, required: true },
+  localGuardian: { type: localGuardianSchema, required: true },
   profileImage: { type: String },
-  isActive: { type: String, enum: ['active', 'blocked'] },
+  isActive: {
+    type: String,
+    enum: {
+      values: ['active', 'blocked'],
+      message: '{VALUE} is not a valid status',
+    },
+    default: 'active',
+  },
 });
 
 const StudentModel = model<Student>('Student', studentSchema);
