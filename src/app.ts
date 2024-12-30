fcc2a9ff02c3caf14b6e43a0d2c210a584e5585d
xeron <rifat.rahman378@gmail.com>
2024-12-31 02:24:07 +0600

    api working succesfully

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { StudentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 // const port = 3000;
 
 //parser
 app.use(express.json());
 app.use(cors());
+
+//application routes
+app.use('/api/v1/students', StudentRoutes);
+
+
+
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,9 +3,9 @@ import { StudentService } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body;
+    const { student: studentData } = req.body;
     //will call service function to send this data
-    const result = await StudentService.createStudentIntoDB(student);
+    const result = await StudentService.createStudentIntoDB(studentData);
     //send response
     res.status(200).json({
       success: true,
@@ -16,7 +16,7 @@ const createStudent = async (req: Request, res: Response) => {
     res.status(500).json({
       success: false,
       message: 'Something went wrong',
-      error: error,
+      error: error instanceof Error ? error : 'Unknown error occurred',
     });
   }
 };
