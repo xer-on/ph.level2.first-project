241b05522af9d63206ac009544de039102979c0c
Xêrøñ <88945309+xer-on@users.noreply.github.com>
2025-03-12 15:46:49 +0600

    new user module added

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,10 @@
+export type TUser = {
+  id: string;
+  password: string;
+  needsPasswordChange: boolean;
+  role: 'admin' | 'faculty' | 'student';
+  status: 'in-progress' | 'blocked';
+  isDeleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
