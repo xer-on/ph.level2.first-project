d2f85d0e42955e357ae8412d207b076672dc877c
Xêrøñ <88945309+xer-on@users.noreply.github.com>
2025-04-20 04:48:04 +0600

    Refactor routing in app.ts: consolidate student and user routes into a single router for cleaner structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import notFound from './app/middleware/notFound';
+import router from './app/routes';
 const app: Application = express();
 // const port = 3000;
 
@@ -11,15 +10,13 @@ app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-
 //not found route
-app.use(notFound)
+app.use(notFound);
 
 export default app;
